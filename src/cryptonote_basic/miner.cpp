943177f22b28cbe516857c4d50c58132e18a5abc
Harrison Hesslink <harrisonhesslink24@gmail.com>
2019-07-19 20:00:21 -0700

    Release Dataset on stop_mining

--- src/cryptonote_basic/miner.cpp
+++ src/cryptonote_basic/miner.cpp
@@ -355,6 +355,7 @@ namespace cryptonote
   {
     boost::interprocess::ipcdetail::atomic_write32(&m_stop, 1);
   }
+  extern "C" void rx_stop_mining(void);
   //-----------------------------------------------------------------------------------------------------
   bool miner::stop()
   {
@@ -383,6 +384,8 @@ namespace cryptonote
 
     MINFO("Mining has been stopped, " << m_threads.size() << " finished" );
     m_threads.clear();
+    m_threads_autodetect.clear();
+    rx_stop_mining();
     return true;
   }
   //-----------------------------------------------------------------------------------------------------
