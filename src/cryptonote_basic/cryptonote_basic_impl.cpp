8fc0cf23e9793b2efb96ea36d531d22a39ad7624
Harrison Hesslink <harrisonhesslink24@gmail.com>
2022-01-07 23:55:02 -0800

    Burn

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -117,9 +117,14 @@ namespace cryptonote {
 
 
      //Adds the burn amount to emission effectively burning the amount added.  
-     if (version > 10) {
+     if (version > 10 && version < 12) {
       already_generated_coins -= MINT_BRIDGE; //subtracts from already_generated_coins as we already burned ~10M
       already_generated_coins += BURN_1; //adds in the burn 
+     }
+
+     if (version > 11)
+     {
+       already_generated_coins += BURN_2;
      }
 
 	   uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
