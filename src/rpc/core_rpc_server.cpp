b800a5955491cae574d8585af690900687c7685d
Harrison Hesslink <harrisonhesslink24@gmail.com>
2022-05-18 14:52:52 -0700

    Update core_rpc_server.cpp

--- src/rpc/core_rpc_server.cpp
+++ src/rpc/core_rpc_server.cpp
@@ -3810,8 +3810,8 @@ namespace cryptonote
         staker_portion = ((reward /2)) * (((res.total_staked_amount / res.total_nodes_staked_to) * 10000) / avg_staking_req);
       }
       res.staker_pool_portion = staker_portion;
-      res.estimated_earnings_for_staking_period_end = (720/pubkey_info_list.size()) * (staker_portion) * (avg_unlock_time - avg_reg_height) * res.total_nodes_staked_to;
-      res.estimated_earnings_daily = (720/pubkey_info_list.size()) * (staker_portion) * res.total_nodes_staked_to;
+      res.estimated_earnings_for_staking_period_end = (720 * 10000/pubkey_info_list.size()) * (staker_portion) * (avg_unlock_time - avg_reg_height) * res.total_nodes_staked_to;
+      res.estimated_earnings_daily = (720 * 10000/pubkey_info_list.size()) * (staker_portion) * res.total_nodes_staked_to;
   }
 
   //------------------------------------------------------------------------------------------------------------------------------
