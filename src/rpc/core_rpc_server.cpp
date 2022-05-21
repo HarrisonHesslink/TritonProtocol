c12208ee1daaf636a933f410e62ab46b00875d34
Harrison Hesslink <harrisonhesslink24@gmail.com>
2022-05-21 12:15:06 -0700

    Update core_rpc_server.cpp

--- src/rpc/core_rpc_server.cpp
+++ src/rpc/core_rpc_server.cpp
@@ -3779,7 +3779,7 @@ namespace cryptonote
           continue;
 
 
-        COMMAND_RPC_GET_STAKER::node n = {string_tools::pod_to_hex(pubkey_info.pubkey), contribute_amount};
+        // COMMAND_RPC_GET_STAKER::node n = {string_tools::pod_to_hex(pubkey_info.pubkey), contribute_amount};
         // res.nodes_staked_to.push_back(n);
 
         avg_unlock_time += pubkey_info.info.registration_height + 20160;
