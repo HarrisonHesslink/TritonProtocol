6c01ea82455febde7a929ec8183eb6be7592736d
Harrison Hesslink <harrisonhesslink24@gmail.com>
2022-07-27 00:04:33 -0700

    Return false for future governance.

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -91,7 +91,7 @@ static_assert(STAKING_PORTIONS % 3 == 0, "Use a multiple of three, so that it di
 #define BURN_1                                          ((uint64_t)70000000000) // 7,000,000 XEQ BURN
 #define MINT_BRIDGE                                     ((uint64_t)167195840000) //16,719,584 XEQ MINT for Bridge
 #define BURN_2                                          ((uint64_t)40000000000) // 7,000,000 XEQ BURN
-
+#define CORP_MINT                                       ((uint64_t)80000000000)
 
 
 #define EMISSION_SPEED_FACTOR_PER_MINUTE                (20)
